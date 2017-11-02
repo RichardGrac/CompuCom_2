e3d48d27dd328553a36d5e126269a7c21773abe3
Ricardo Garcia <Richard_Grac@hotmail.com>
2017-11-01 23:10:49 -0600

    Error pages & Login page

--- src/main/java/com/example/CompuCom2/Constants/Constants.java
+++ src/main/java/com/example/CompuCom2/Constants/Constants.java
@@ -6,4 +6,5 @@ public class Constants {
     public static final String USER_FORM = "userform";
     public static final String PRODUCT_FORM = "productform";
     public static final String CATEGORY_VIEW = "categories";
+    public static final String LOGIN = "login";
 }
---
+++ src/main/java/com/example/CompuCom2/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.example.CompuCom2.controller;
+
+import com.example.CompuCom2.Constants.Constants;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping("")
+    public ModelAndView login(){
+        ModelAndView mav = new ModelAndView(Constants.LOGIN);
+        return mav;
+    }
+}
