0a1e4293ffedc89eaaf63149eadcb3d4d936a084
Ricardo Garcia <Richard_Grac@hotmail.com>
2017-11-22 20:51:17 -0600

    Pending Shippings working.

--- src/main/java/com/example/CompuCom2/Constants/Constants.java
+++ src/main/java/com/example/CompuCom2/Constants/Constants.java
@@ -25,5 +25,6 @@ public class Constants {
     public static final String SEARCHSHIPPING = "searchshipping";
     public static final String SFINISHED = "sfinished";
     public static final String SALL = "sall";
+    public static final String SPENDING = "spending";
     public static final String FORM_USER_PASSWORD = "front/form-password";
 }
